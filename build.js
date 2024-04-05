6b3625f2ecb6726f5c74912955eb05d1a9cd93e3
Trevor Morris <code@trovster.com>
2024-04-05 13:56:34 +0100

    ci: refactor build script

--- build.js
+++ build.js
@@ -1,16 +1,41 @@
-const fs = require('fs');
-const jsDir = './build/js';
-const cssDir = './build/css';
+const fs = require('fs-extra');
 
-if (! fs.existsSync(jsDir)) {
-  fs.mkdirSync(jsDir, { recursive: true });
+const src = './src';
+const build = './build';
+
+const dirs = {
+  js: `${build}/js`,
+  css: `${build}/css`
+};
+
+const origination = {
+  js: './node_modules/@stoplight/elements/web-components.min.js',
+  css: './node_modules/@stoplight/elements/styles.min.css',
+  spec: './openapi.yaml',
+};
+
+const destination = {
+  js: `${dirs.js}/site.js`,
+  css: `${dirs.css}/site.css`,
+  spec: `${build}/openapi.yaml`,
+};
+
+if (! fs.existsSync(dirs.js)) {
+  fs.mkdirSync(dirs.js, { recursive: true });
 }
 
-if (! fs.existsSync(cssDir)) {
-  fs.mkdirSync(cssDir, { recursive: true });
+if (! fs.existsSync(dirs.css)) {
+  fs.mkdirSync(dirs.css, { recursive: true });
 }
 
-fs.copyFileSync('./node_modules/@stoplight/elements/web-components.min.js', './build/js/site.js')
-fs.copyFileSync('./node_modules/@stoplight/elements/styles.min.css', './build/css/site.css');
-fs.copyFileSync('./openapi.yaml', './build/openapi.yaml');
-fs.copyFileSync('./src/index.html', './build/index.html');
+fs.copy(src, build, { recursive: true });
+
+Object.entries(destination).forEach(([key, value]) => {
+  if (fs.existsSync(value)) {
+    fs.unlinkSync(value);
+  }
+
+  fs.copy(origination[key], value);
+});
+
+console.log('Build complete.');
